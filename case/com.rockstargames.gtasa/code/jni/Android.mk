08093844fd7a54f0fb7a5ff0bc492af202b69986
havenow <715667426@qq.com>
2020-08-23 17:42:08 +0800

    通过socket 使能cheat

--- case/com.rockstargames.gtasa/code/jni/Android.mk
+++ case/com.rockstargames.gtasa/code/jni/Android.mk
@@ -5,6 +5,7 @@ LOCAL_MODULE    := hook
 LOCAL_SRC_FILES := inlineHook.c relocate.c
 include $(BUILD_STATIC_LIBRARY)
 
+#--------------------------------------------------------
 include $(CLEAR_VARS)
 LOCAL_MODULE    := inlineHook
 LOCAL_SRC_FILES := cheatMain.cpp
@@ -13,3 +14,17 @@ LOCAL_STATIC_LIBRARIES := libhook
 #LOCAL_CFLAGS := -funwind-tables
 LOCAL_LDLIBS := -llog 
 include $(BUILD_SHARED_LIBRARY)
+
+#--------------------------------------------------------
+include $(CLEAR_VARS)
+LOCAL_MODULE    := cheatJ
+LOCAL_SRC_FILES := cheatMain.cpp
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_STATIC_LIBRARIES := libhook
+#LOCAL_CFLAGS := -funwind-tables
+LOCAL_LDLIBS := -llog 
+
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
+
+include $(BUILD_EXECUTABLE)
--- case/com.rockstargames.gtasa/code/jni/cheatMain.cpp
+++ case/com.rockstargames.gtasa/code/jni/cheatMain.cpp
@@ -3,9 +3,147 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <android/log.h>
+#include <pthread.h>
+
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
 
 #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,"inlinehook",__VA_ARGS__)
 
+char g_szDefaultSocketName[64] = "cheat";
+int g_socket = 0;
+int g_port = 1166;
+bool g_bCallWeaponCheat1 = false;
+
+int start_server(char* sockname)
+{
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (fd < 0)
+	{
+		LOGE("creating socket");
+		return fd;
+	}
+
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(g_port);
+
+	int opt;
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (void*)&opt, sizeof(opt));
+
+	if (bind(fd, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0)
+	{
+		LOGE("binding socket");
+		close(fd);
+		return -1;
+	}
+
+	listen(fd, 1);
+
+	return fd;
+}
+
+void process_cmd(char* io_buffer)
+{
+	LOGE("process_cmd io_buffer:	%s\n", io_buffer);
+	int index;
+	char* nextPtr;
+	void* addr;
+	int data;
+	char* cursor = io_buffer;
+	cursor += 1;
+	int status;
+	switch (io_buffer[0])
+	{
+	case 'D':
+		LOGE("process_cmd D:	%s\n", io_buffer);
+		index = strtol(cursor, &cursor, 10);
+		LOGE("index:	%d\n", index);
+		g_bCallWeaponCheat1 = true;
+		break;
+	default:
+		break;
+	}
+}
+
+
+static pthread_t cheat_thread;
+void* threadProc(void* param)
+{
+	char* sockname = g_szDefaultSocketName;
+	struct sockaddr_un client_addr;
+	socklen_t client_addr_length = sizeof(client_addr);
+
+	int server_fd = start_server(sockname);
+	if (server_fd < 0)
+	{
+		LOGE("start server failed:Unable to start server on %s\n", sockname);
+		LOGE("maybe have already start a server.\n");
+		return NULL;
+	}
+	LOGE( "start server succeeded\n");
+	while (1)
+	{
+		int client_fd = accept(server_fd, (struct sockaddr*) NULL, NULL);
+
+		if (client_fd < 0)
+		{
+			LOGE("accepting client exit(1)\n");
+			//exit(1);
+			continue;
+		}
+
+		LOGE("Connection established\n");
+
+		char io_buffer[80] = { 0 };
+		int io_length = 0;
+		char* cursor;
+		long int contact, x, y, pressure;
+
+		while (1) {
+			io_length = 0;
+
+			while (io_length < sizeof(io_buffer)
+					&& recv(client_fd, &io_buffer[io_length], 1, 0) == 1) {
+				if (io_buffer[io_length++] == '\n') {
+					break;
+				}
+			}
+
+			if (io_length <= 0) {
+				LOGE("judgement: client %d have exited.\n", client_fd);
+				break;
+			}
+
+			if (io_buffer[io_length - 1] != '\n') {
+				continue;
+			}
+
+			if (io_length == 1) {
+				continue;
+			}
+
+			process_cmd(io_buffer);
+		}
+
+		LOGE("client_fd %d Connection closed\n", client_fd);
+		close(client_fd);
+	}
+
+	close(server_fd);
+	LOGE("exit cheat thread\n");
+    return NULL;
+}
 
 
 #include <unwind.h>
@@ -128,6 +266,16 @@ int find_pid_of(const char *process_name)
 #include "inlineHook.h"
 //enum ele7en_status registerInlineHook(uint32_t target_addr, uint32_t new_addr, uint32_t **proto_addr)
 
+#ifndef _TAG_CHEAT_UNIT_
+#define _TAG_CHEAT_UNIT_
+typedef struct tagCheatUnit
+{
+	char strCheatName[128];
+	int (*pfunCheat)();
+	bool bCall;
+} CheatUnit;
+#endif
+
 int (*old_WeaponCheat1)(/*void* CCheatThis*/) = NULL;
 int replaceWeaponCheat1(/*void* CCheatThis*/)
 {
@@ -157,11 +305,16 @@ int hookWeaponCheat1()
 int (*old_GameProcess)() = NULL;
 int replaceGameProcess()
 {
-	LOGE("replace GameProcess\n");
+	//LOGE("replace GameProcess\n");
 	int ret = old_GameProcess();
-	LOGE("replace GameProcess return: %d\n", ret);
+	//LOGE("replace GameProcess return: %d\n", ret);
 	
-	f__ZN6CCheat12WeaponCheat1Ev();
+	if (g_bCallWeaponCheat1)
+	{
+		f__ZN6CCheat12WeaponCheat1Ev();
+		g_bCallWeaponCheat1 = false;
+	}
+
 	return ret;
 }
 int hookGameProcess()
@@ -289,6 +442,80 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved)
 	funGameProcessdAddr = (long)(info.dli_saddr);
 	hookGameProcess();
 
+//create cheat thread-------------------------------------------------------------------------------------------------------	
+	LOGE("create cheat thread.\n");
+	pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    pthread_create(&cheat_thread, &attr, threadProc, NULL);
+
 	result = JNI_VERSION_1_4;
 	return result;
+}
+
+void printhelp()
+{
+	fprintf(stderr, 
+		"####################################################\n"
+		"				Usage: input cmd\n"
+		"help\n"
+		"exit\n"
+		"1	(cheatWeapon1)\n"
+		"####################################################\n"
+		"\n");
+}
+
+int main(int argc, char** argv)
+{
+	struct sockaddr_in addr;
+	g_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (g_socket < 0)
+	{
+		fprintf(stderr, "creating socket error\n");
+		return 1;
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET; //设置为IP通信
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(g_port);
+
+	if (connect(g_socket, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+	{
+		fprintf(stderr, "connect socket error %d\n", errno);
+		close(g_socket);
+		return 1;
+	}
+	fprintf(stderr, "connect socket sucess %d\n", g_socket);
+	
+	printhelp();
+	while (1)
+	{
+		fprintf(stderr, "input commonds>");
+		char cmdBuf[128] = {};
+		scanf("%s", cmdBuf);
+		if (0 == strcmp(cmdBuf, "exit"))
+		{
+			fprintf(stderr, "exit cmd\n");
+			break;
+		}
+		else if (0 == strcmp(cmdBuf, "help"))
+		{
+			printhelp();
+		}
+		else
+		{
+			int cheatIndex = 0;
+			sscanf(cmdBuf, "%d", &cheatIndex);
+			LOGE("cheat index %d\n", cheatIndex);
+			char cmd[80] = { 0 };
+			int io_length = snprintf(cmd, sizeof(cmd), "D %d\n", cheatIndex);
+			send(g_socket, cmd, io_length, 0);
+		}
+	}
+	
+	
+	fprintf(stderr, "socket close %d\n", g_socket);
+	close(g_socket);
+	return 0;
 }
